4da12e6961352e1e31f51aedefca0f274c1af23a
Ryan M Hufford <ryan@rmhufford.com>
2021-05-01 11:03:34 -0400

    checkpoint

---
+++ time-converter/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClockComponent } from './clock/clock.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClockComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
